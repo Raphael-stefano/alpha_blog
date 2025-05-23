0aae2a820cc96813aed6f3fc7225506d8004e2fe
Raphael-stefano <raphaelestefano@gmail.com>
2025-05-23 03:06:18 -0300

    styled articles pages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+
+  validates :title, length: { minimum: 6, maximum: 300 }
+  validates :description, length: { minimum: 10, maximum: 10000 }
 end
